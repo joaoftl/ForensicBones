2aff1217b3f8702b234fa6706fc4dde965ff8c5e
João Paulo de Freitas Teixeira Leite <joao.leite.740407@sga.pucminas.br>
2023-11-15 16:10:35 -0300

    Relatorios Controller adicionado

---
+++ ForensicBones/Controllers/RelatoriosController.cs
@@ -0,0 +1,21 @@
+﻿using ForensicBones.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ForensicBones.Controllers
+{
+    public class RelatoriosController : Controller
+    {
+        private readonly AppDbContext _context; 
+        public RelatoriosController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var dados = await _context.Relatorios.ToListAsync();
+            return View(dados);
+        }
+    }
+}
